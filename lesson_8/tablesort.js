dd7de48bc9f64628f9090f12e2d4b6005ac83904
anwarmoazam <anwar.palimarwar@gmail.com>
2022-11-08 23:23:27 +0530

    sorting table project

---
+++ lesson_8/tablesort.js
@@ -0,0 +1,9 @@
+function sortTableByColumn(table, column, asc = true) {
+    const dirModifier = asc ? 1 : -1;
+    const tBody = table.tBodies[0];
+    const rows = Array.from(tBody.querySelectorAll('tr'));
+    console.log(dirModifier);
+    console.log(tBody);
+    console.log(rows);
+}
+
