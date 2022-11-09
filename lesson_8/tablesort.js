7451f829a965d75af84122e5681afd2990dac15d
anwarmoazam <anwar.palimarwar@gmail.com>
2022-11-09 17:44:44 +0530

    project table sort

--- lesson_8/tablesort.js
+++ lesson_8/tablesort.js
@@ -2,8 +2,21 @@ function sortTableByColumn(table, column, asc = true) {
     const dirModifier = asc ? 1 : -1;
     const tBody = table.tBodies[0];
     const rows = Array.from(tBody.querySelectorAll('tr'));
-    console.log(dirModifier);
-    console.log(tBody);
-    console.log(rows);
+
+    // Sort each row
+    const sortedRows = rows.sort((a,b) => {
+        const aColText = a.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
+        const bColText = a.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
+
+        console.log(aColText);
+        console.log(bColText);
+
+    })
 }
 
+sortTableByColumn(document.querySelector('table'),1);
+
+// let anwarTable = document.querySelector('table');
+// console.log(anwarTable.tBodies[0]);
+// let arr = anwarTable.tBodies[0].querySelector('tr');
+// console.log(arr)
